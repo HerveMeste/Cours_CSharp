dd0af002acead0985c6a8a07857232b9c5fea61f
HerveMeste <herve.meste@gmail.com>
2021-03-17 22:36:55 +0100

    Reflexion

---
+++ Bases/Reflexion/Inspector.cs
@@ -0,0 +1,60 @@
+﻿using ReflectionTest;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Reflexion
+{
+    internal class Inspector
+    {
+        private Type _objectType;
+        public Inspector(Type objectType)
+        {
+            _objectType = objectType;
+        }
+        internal void GetAllProperties()
+        {
+            
+            PropertyInfo[] properties = _objectType.GetProperties(BindingFlags.NonPublic | BindingFlags.Instance);
+            Console.WriteLine("Properties");
+            foreach (var p in properties)
+            {
+                Console.WriteLine("\t" + p);
+            }
+        }
+
+        internal void GetAllFields()
+        {
+            
+            FieldInfo[] fields = _objectType.GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+            Console.WriteLine("Fields");
+            foreach (var f in fields)
+            {
+                Console.WriteLine("\t" + f);
+            }
+        }
+
+        internal void GetAllConstructors()
+        {
+            
+            ConstructorInfo[] constructors = _objectType.GetConstructors(BindingFlags.NonPublic | BindingFlags.Instance);
+            Console.WriteLine("Constructors");
+            foreach (var c in constructors)
+            {
+                Console.WriteLine("\t" + c);
+            }
+        }
+
+        internal void GetAllMethods()
+        {
+            
+            MethodInfo[] methods = _objectType.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance);
+            Console.WriteLine("Methods");
+            foreach (var m in methods)
+            {
+                Console.WriteLine("\t" + m);
+            }
+        }
+    }
+}
---
+++ Bases/Reflexion/Program.cs
@@ -0,0 +1,21 @@
+﻿using Reflexion;
+using System;
+using System.Reflection;
+
+namespace ReflectionTest
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reflected = new ReflectedClass();
+            Type objectType = reflected.GetType();
+            var inspector = new Inspector(objectType);
+            inspector.GetAllProperties();
+            inspector.GetAllFields();
+            inspector.GetAllMethods();
+            inspector.GetAllConstructors();
+        }
+    }
+}
