b2b8911416d55ed5220a9dd6d8e782eb7a78bed5
HerveMeste <herve.meste@gmail.com>
2021-03-17 22:08:16 +0100

    ASP MVC

---
+++ AspNet/ASPMVC/Data/Context.cs
@@ -0,0 +1,15 @@
+﻿using ASPMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MvcMovie.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
