9b4d60ebfeddd302304c4c7853c4d0de6130bee3
HerveMeste <herve.meste@gmail.com>
2021-03-17 22:52:18 +0100

    Async_Await

---
+++ Bases/Async_Await/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace async_await
+{
+    class Program
+    {
+        static async Task<Int32> Main(string[] args)
+        {
+            var taskOne = NewMethod();
+            var taskTwo = NewMethod();
+
+            await Task.WhenAll(taskOne, taskTwo);
+            Console.WriteLine("Finished");
+            return 0;
+        }
+
+        private static async Task NewMethod()
+        {
+            Random rng = new Random();
+            Int32 waitTime = rng.Next(1000, 5000);
+            await Task.Delay(waitTime);
+            Console.WriteLine("The task has waited " + waitTime);
+        }
+    }
+}
