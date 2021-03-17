70c4343026c8c63371b9bae380c975fcced02d0e
HerveMeste <herve.meste@gmail.com>
2021-03-17 22:26:28 +0100

    ASPNET_Routage

---
+++ AspNet/ASPNET_Routage/Controllers/HelloAttributesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNET_Routage.Controllers
+{
+    /*[Route("api/[controller]")]*/
+    [ApiController]
+    public class HelloAttributesController
+    {
+        [Route("Say")] // The endpoint here is reached by calling `/Say` route
+        public String SayHello()
+        {
+            return "Hello via attribute !";
+        }
+
+        [Route("Yell")] // The endpoint here is reached by calling `/Yell` route
+        public String YellHello()
+        {
+            return "HELLO ATTRIBUTE !";
+        }
+    }
+}
---
+++ AspNet/ASPNET_Routage/Controllers/HelloController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNET_Routage.Controllers
+{
+    public class HelloController
+    {
+        public String SayHello()
+        {
+            return "Hello !";
+        }
+
+        public String YellHello()
+        {
+            return "HELLO !";
+        }
+    }
+
+
+}
